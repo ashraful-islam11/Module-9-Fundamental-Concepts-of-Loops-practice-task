517b5fa98dee9cc4c7210c826b110ae7830cb555
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-19 18:19:56 +0600

    task-2 : print numbers 1 to 10

---
+++ task-2.js
@@ -0,0 +1,7 @@
+// todo:                Task 2 —   topic:  Print Numbers 1 to 10 
+
+// 1. Write a for loop that prints numbers from 1 to 10.
+
+for(let number = 1; number <=10; number++){
+    console.log('the number of value :', number);
+}
