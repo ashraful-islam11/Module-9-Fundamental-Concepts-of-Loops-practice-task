020121c9d4b720b36befc9452bbb7b37bfbb0124
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-19 21:56:37 +0600

    task 12 :Do while loop - optional

---
+++ task-12.js
@@ -0,0 +1,21 @@
+/**
+ * todo:        Task 12 — Do While Loop — Optional 
+ * Write a do while loop that prints numbers from 1 to 5. Then modify the condition so it is false from the start (for example, i > 10) and observe that the loop still runs once.      */
+
+let i = 1;
+
+// step 1:   condition true :
+
+do{
+    console.log(i);
+    i++;
+
+}while ( i <= 5);
+
+
+// step 2 : condition false .
+let n = 1;
+ do{
+    console.log(n);
+    n++;
+ }while(n > 10);
