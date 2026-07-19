0f5da1a4616a8d22b3ad1e1cb3283cb6dd54d79e
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-19 18:46:06 +0600

    task- 6: sum of numbers

---
+++ task-6.js
@@ -0,0 +1,14 @@
+/**
+ * todo:            Task 6 — Sum of Numbers 
+ * Write a for loop that calculates the sum of numbers from 1 to 20 and prints the result.        */
+
+
+console.log('use for loop  calculates sum of 1-20 : ');
+
+let sum = 0 ;
+for( let number = 1; number <= 20 ; number++){
+     sum = sum + number;
+     console.log(sum);
+}
+
+
