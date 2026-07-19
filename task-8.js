be25d0a6d7012dca2c0f7bf2fa0234a513325710
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-19 18:55:29 +0600

    task -8 : count down form 10

---
+++ task-8.js
@@ -0,0 +1,10 @@
+/**
+ * todo:            Task 8 — Count Down from 20 
+ * Write a for loop that counts down from 20 to 1.                  */
+
+console.log('count down start: ');
+for( let count = 20; count >= 1 ; count--){
+    console.log(count);
+}
+
+console.log('Boooooo---MMMMM ✨🎈✨');
