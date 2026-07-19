326d12d72efc7b34a70f6a5e830537b058d0c37f
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-19 18:13:08 +0600

    print hello five times

---
+++ task-1.js
@@ -0,0 +1,7 @@
+ // todo:       topic:          Task 1 — Print "Hello" Five Times
+
+// 1.  Write a for loop that prints "Hello" 5 times.
+
+for(let number = 1; number<= 5; number ++){
+    console.log('Hello -',number);
+}
