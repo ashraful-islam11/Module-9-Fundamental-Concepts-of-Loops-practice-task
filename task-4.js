d543edd803db8fc00bb3058d1eb0abc9cdae5357
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-19 18:34:27 +0600

    task-4 Repeat a Message

---
+++ task-4.js
@@ -0,0 +1,10 @@
+// todo:        task - 4    Topic:   Repeat a Message 
+
+    // Using a while loop, print "Loop চলছে" exactly 7 times.
+
+
+    let loopRun = 1;
+    while(loopRun <= 7){
+        console.log('Loop চলছে -', loopRun);
+        loopRun ++;
+    }
