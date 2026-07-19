aedcf5dcbb5cb5e7a34c55581c8a80914e10db09
ashrafulislam2k6 <ashraful2742k6@gmail.com>
2026-07-19 18:26:31 +0600

    task-3 : countDown whit while loop

---
+++ task-3.js
@@ -0,0 +1,16 @@
+// todo:        task- 3         topic:  Countdown with While Loop 
+
+    // 1. Write a while loop that prints numbers from 10 down to 1.
+
+    let Countdown = 10;
+
+    console.log('count down start:');
+
+    while(Countdown >= 1){   
+
+        console.log(Countdown);
+
+        Countdown--;
+    }
+    console.log('Bo======oM 🏆');
+
